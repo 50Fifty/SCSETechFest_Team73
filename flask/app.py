590fa600e69375e4c450f60fa8ff5496920e8c0d
50Fifty <jenseanfoo@gmail.com>
2023-01-16 01:32:44 +0800

    Setup AJAX

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
+import json
 
 app = Flask(__name__)
 
@@ -9,9 +10,30 @@ def homepage():
 @app.route("/api/getQuestion", methods=["POST"])
 def getQuestion():
     """
+    TODO
     Add code here to get question.
     """
-    return {"question" : "abcdef"}
+    # return {"question" : "abcdef?"}
+    response = Response(
+        response=json.dumps({"question": "abcdefg?"}),
+        mimetype="application/json",
+        status=200
+    )
+    return response
+
+@app.route("/api/receiveAnswer", methods=["POST"])
+def receiveAnswer():
+    """
+    """
+    if "question" in request.json and "answer" in request.json:
+        qn, ans = request.json["question"], request.json["answer"]
+        """
+        TODO
+        Add code here to return question and answer back to model
+        """
+        return {"success": True}
+    else:
+        return ("Invalid parameters", 400)
 
 if __name__ == "__main__":
     app.run()
