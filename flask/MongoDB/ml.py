a7043ae683f1ecf79d349265d4c928a9ac46e4b5
anthonychew <anthony.chew@hotmail.com>
2023-01-16 21:22:44 +0800

    Update question api to post and get

--- flask/MongoDB/ml.py
+++ flask/MongoDB/ml.py
@@ -5,6 +5,22 @@ class ml:
     def __init__(self):    
 
         dbname = get_database()
+        # dbname = dbname.create_collection("ML", validator={
+        #     '$jsonSchema': {
+        #         'bsonType': 'object',
+        #         'additionalProperties': True,
+        #         'required': ['component', 'path'],
+        #         'properties': {
+        #             'component': {
+        #                 'bsonType': 'string'
+        #             },
+        #             'path': {
+        #                 'bsonType': 'string',
+        #                 'description': 'Set to default value'
+        #             }
+        #         }
+        #     }
+        # })
         self.db = dbname["ML"]
 
     def get(self):
@@ -13,6 +29,8 @@ class ml:
     def get(self , role):
         return self.db.find_one({"role":role})
 
+    def create(self,role):
+        return self.db.insert_one(role)
     
     def put(self , role , answer):
         self.db.find_one_and_update({'role':role}, { '$set': { "answer" : answer} } )
